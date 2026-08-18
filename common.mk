f13e21ad29cb296afcdb097074d42d81f85f4211
IRonSquidist <96339831+zhafknight@users.noreply.github.com>
2026-08-18 20:33:09 +0800

    smdk4412-common: Drop Samsung Service Mode
    Removed SamsungServiceMode from PRODUCT_PACKAGES.

--- common.mk
+++ common.mk
@@ -112,10 +112,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     SamsungDoze
 
-# SamsungServiceMode
-PRODUCT_PACKAGES += \
-    SamsungServiceMode
-
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
