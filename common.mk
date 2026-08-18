5363aab1c512211e8d20761e26a298b878c68c0d
IRonSquidist <96339831+zhafknight@users.noreply.github.com>
2026-08-18 20:45:16 +0800

    smdk4412-common: Update vendor.mk path

--- common.mk
+++ common.mk
@@ -407,7 +407,7 @@ $(call inherit-product, hardware/samsung/exynos4x12.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_k.mk)
 
 # Include non-opensource parts
-$(call inherit-product, vendor/samsung/smdk4412-common-treble/smdk4412-common-vendor.mk)
+$(call inherit-product, vendor/samsung/smdk4412-common/smdk4412-common-vendor.mk)
 
 # Art
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
