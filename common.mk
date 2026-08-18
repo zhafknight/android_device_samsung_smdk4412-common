cbb8cf593dca67485f584aaa70fc79de372bbb5c
IRonSquidist <96339831+zhafknight@users.noreply.github.com>
2026-08-18 20:37:08 +0800

    smdk4412-common: Use Aperture for Camera apps

--- common.mk
+++ common.mk
@@ -175,7 +175,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.5-service \
     camera.device@1.0-impl \
-    Camera2
+    Aperture
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/80cfw:vendor/etc/init.d/80cfw
